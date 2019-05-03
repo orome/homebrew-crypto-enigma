84717dea3df2428961db17adb6322a214dc06b9d
Roy Levien <royl@aldaron.com>
2019-05-03 11:16:20 -0400

    Add some tests

--- crypto-enigma.rb
+++ crypto-enigma.rb
@@ -1,11 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CryptoEnigma < Formula
   desc "Enigma machine executable for macOS"
   homepage ""
   url "https://github.com/orome/crypto-enigma-macos/archive/0.1.1.5.tar.gz", :using => :curl
-
   sha256 "5327bdcf1bec73339339bf9c0f03b540cbea2149e50066c953b224d7f1cfae53"
 
   #version "0.1.1.5"
@@ -19,17 +17,9 @@ class CryptoEnigma < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test crypto-enigma-macos`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     assert_match "0.1.1.5", shell_output("#{bin}/enigma --version") 
-    #system "false"
+    assert_match "OZQK PFLP YZRP YTFV U", shell_output("#{bin}/enigma encode 'B-I-III-I EMO UX.MO.AY 13.04.11' 'TESTINGXTESTINGUD'") 
+    assert_match "OZQK PFLP YZRP YTFV U", shell_output("#{bin}/enigma encode 'B-I-III-I EMO UX.MO.AY 13.04.11' 'TESTING! testing?'") 
   end
 end
 
