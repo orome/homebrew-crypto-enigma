b027c1685572b6a28bc3830b9d8ca9cfdb323763
Roy Levien <royl@aldaron.com>
2019-10-09 13:59:05 -0400

    Update to latest Hackage release

--- crypto-enigma.rb
+++ crypto-enigma.rb
@@ -3,10 +3,10 @@
 class CryptoEnigma < Formula
   desc "Enigma machine executable for macOS"
   homepage ""
-  url "https://github.com/orome/crypto-enigma-macos/archive/0.1.1.5.tar.gz", :using => :curl
-  sha256 "5327bdcf1bec73339339bf9c0f03b540cbea2149e50066c953b224d7f1cfae53"
+  url "https://github.com/orome/crypto-enigma-macos/archive/0.1.1.6.tar.gz", :using => :curl
+  sha256 "9270960f03950141373c1987b997e6119351426bee610612c9cbd9a16db9dd09"
 
-  #version "0.1.1.5"
+  #version "0.1.1.6"
   #url "git@github.com:orome/crypto-enigma-macos.git", :using => :git
   #url "https://github.com/orome/crypto-enigma-macos.git"#, :using => :git  
 
@@ -17,7 +17,7 @@ class CryptoEnigma < Formula
   end
 
   test do
-    assert_match "0.1.1.5", shell_output("#{bin}/enigma --version") 
+    assert_match "0.1.1.6", shell_output("#{bin}/enigma --version") 
     # Update these tests when bug is fixed: https://github.com/orome/crypto-enigma-hs/issues/33
     assert_match "OZQK PFLP YZRP YTFV U", shell_output("#{bin}/enigma encode 'B-I-III-I EMO UX.MO.AY 13.04.11' 'TESTINGXTESTINGUD'") 
     assert_match "OZQK PFLP YZRP YTFV U", shell_output("#{bin}/enigma encode 'B-I-III-I EMO UX.MO.AY 13.04.11' 'TESTING! testing?'") 
